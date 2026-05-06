9a452cd15c499927196afedf3dbd6bc301fe97e2
oaimport741-lgtm <oaimport741@gmail.com>
2026-05-06 16:53:18 -0300

    Update mileage-config.js

--- mileage-config.js
+++ mileage-config.js
@@ -1,5 +1,5 @@
 window.OA_MILEAGE_CONFIG = {
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwi8x1kMDYICu0apXyNSWpLVTPhXOVqAOSvJEu9xejnv_ZvEaXo5VidanrUPl7OPztG0g/exec",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbz9swwpLW_qABFfwB9ZuAah9Hn3iixD8xx_u_W2O-rq7yQJ0cxXPXxH7XoHBW48p6d_6g/exec",
   routeViewerBaseUrl: "https://oaimport741-lgtm.github.io/ControlKM/route.html",
   googleMapsApiKey: "AIzaSyDeXCEigyw0U70YsJiZ7hb0baTsKG0pj9E"
 };
